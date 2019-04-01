d2239f277dfd1a870cbae7799f1d9825424a6753
Jorge Saldivar <Jorge Saldivar>
2019-04-01 18:06:28 +0200

    Add swagger

--- backend/sci_impact/urls.py
+++ backend/sci_impact/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_swagger.views import get_swagger_view
 from . import views
 
+schema_view = get_swagger_view(title='Scientific Impact API')
 
 urlpatterns = [
-    path('scientists/', views.ScientistList.as_view(), name='snippet-list'),
+    path('', schema_view),
+    path('scientists/', views.ScientistList.as_view()),
     path('scientists/<int:pk>/', views.ScientistDetail.as_view()),
     path('api-auth/', include('rest_framework.urls')),
 ]
