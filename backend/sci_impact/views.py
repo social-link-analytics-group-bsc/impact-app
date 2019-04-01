13486d1bbab24ed0cf678f56e9800cb605a2fdcc
Jorge Saldivar <Jorge Saldivar>
2019-04-01 18:06:44 +0200

    Add documentation

--- backend/sci_impact/views.py
+++ backend/sci_impact/views.py
@@ -4,31 +4,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from sci_impact.models import Scientist
 from sci_impact.serializers import ScientistSerializer
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
 
 
 #def index(request):
 #    return HttpResponse("Hello, this app aim at computing the impact of scientific publications.")
 
 
-#@api_view(['GET'])
-#def api_root(request, format=None):
-#    return Response({
-#        'scientists': reverse('snippet-list', request=request, format=format)
-#    })
-
-
 class ScientistList(APIView):
-    """
-    List all scientists, or create a new scientist.
-    """
+
     def get(self, request, format=None):
+        """
+            List all scientists
+        """
         snippets = Scientist.objects.all()
         serializer = ScientistSerializer(snippets, many=True)
         return Response(serializer.data)
 
     def post(self, request, format=None):
+        """
+            Create a new scientist
+        """
         serializer = ScientistSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -37,9 +32,7 @@ class ScientistList(APIView):
 
 
 class ScientistDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
+
     def get_object(self, pk):
         try:
             return Scientist.objects.get(pk=pk)
@@ -47,11 +40,18 @@ class ScientistDetail(APIView):
             raise Http404
 
     def get(self, request, pk, format=None):
+        """
+        Get the scientist identified with the id
+        """
+
         snippet = self.get_object(pk)
         serializer = ScientistSerializer(snippet)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
+        """
+        Update the scientist identified with the id
+        """
         snippet = self.get_object(pk)
         serializer = ScientistSerializer(snippet, data=request.data)
         if serializer.is_valid():
@@ -60,6 +60,9 @@ class ScientistDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
+        """
+        Delete the scientist identified with the id
+        """
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
