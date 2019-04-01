c4e3b5df13a7e6e9facd0486832210bacd611c3e
Jorge Saldivar <Jorge Saldivar>
2019-04-01 17:52:37 +0200

    Remove sensitve information

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'impact-app',
-        'USER': 'impact',
-        'PASSWORD': 'impactapp',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
