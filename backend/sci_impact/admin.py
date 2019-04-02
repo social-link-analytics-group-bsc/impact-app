5d57e7625192acaf8a0f84b601ea4af8414c38a2
Jorge Saldivar <Jorge Saldivar>
2019-04-02 10:11:49 +0200

    Add affiliation admin model

--- backend/sci_impact/admin.py
+++ backend/sci_impact/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from sci_impact.models import Scientist, Country, Institution, City, Region
+from sci_impact.models import Scientist, Country, Institution, City, Region, Affiliation
 
 
+@admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name', 'iso_code')
     #actions = ['load_countries']
@@ -20,27 +21,28 @@ class CountryAdmin(admin.ModelAdmin):
     load_countries.short_description = 'Load list of countries'
 
 
+@admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ('name', 'country')
 
 
+@admin.register(City)
 class CityAdmin(admin.ModelAdmin):
     list_display = ('name', 'region', 'country', 'wikipage')
 
 
+@admin.register(Scientist)
 class ScientistAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'gender')
     ordering = ('last_name', )
     search_fields = ('first_name', 'last_name',)
 
 
+@admin.register(Institution)
 class InstitutionAdmin(admin.ModelAdmin):
     list_display = ('name', 'country', 'city', 'web_page')
 
 
-admin.site.register(Country, CountryAdmin)
-admin.site.register(Region, RegionAdmin)
-admin.site.register(City, CityAdmin)
-admin.site.register(Scientist, ScientistAdmin)
-admin.site.register(Institution, InstitutionAdmin)
-
+@admin.register(Affiliation)
+class AffiliationAdmin(admin.ModelAdmin):
+    list_display = ('scientist', 'institution', 'data_joined')
