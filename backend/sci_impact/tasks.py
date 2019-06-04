4e2b4a1264674364196ea0dd745333b5f81e60e7
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-06-04 17:54:04 +0200

    First version of the function that collects references

--- backend/sci_impact/tasks.py
+++ backend/sci_impact/tasks.py
@@ -65,6 +65,60 @@ def get_citations(article_ids):
 
 
 @shared_task
+def get_references(article_ids):
+    logger.info(f"Starting the process of getting references...")
+    ec = EntrezClient()
+    am = ArticleMgm()
+    references_objs = []
+    num_references = 0
+    for article_id in article_ids:
+        article_obj = Article.objects.get(id=article_id)
+        saved_reference_author, saved_reference_authorship = False, False
+        authorships = article_obj.authorship_set.all()
+        logger.info(f"Getting the references of the paper {article_obj.title}")
+        repo_id = article_obj.repo_id.value
+        paper_references = ec.get_paper_citations(repo_id)
+        logger.info(f"Found {len(paper_references)} citations for the paper")
+        for i, paper_reference in enumerate(paper_references):
+            article_citation_obj, created_objs = am.process_paper(i, paper_reference)
+            if article_citation_obj:
+                try:
+                    ArtifactCitation.objects.get(from_artifact=article_citation_obj,
+                                                 to_artifact=article_obj)
+                    logger.info('Citation already exists!')
+                except ArtifactCitation.DoesNotExist:
+                    # 1) Create citation
+                    citation_obj = ArtifactCitation(from_artifact=article_citation_obj,
+                                                    to_artifact=article_obj)
+                    citation_obj.save()
+                    citation_objs.append(citation_obj)
+                    logger.info('Citation created!')
+                    num_citations += 1
+                    # 2) Update scientist citation metrics
+                    for authorship in authorships:
+                        author = authorship.author
+                        author.article_citations += 1
+                        author.total_citations += 1
+                        if not saved_citation_author:
+                            author.articles_with_citations += 1
+                            saved_citation_author = True
+                        author.save()
+                        # 3) Update affiliation citation metrics
+                        affiliations = Affiliation.objects.filter(scientist=author,
+                                                                  institution=authorship.institution)
+                        for affiliation in affiliations:
+                            affiliation.article_citations += 1
+                            affiliation.total_citations += 1
+                            if not saved_citation_authorship:
+                                affiliation.articles_with_citations += 1
+                                saved_citation_authorship = True
+                            affiliation.save()
+        else:
+            logger.info(f"Could not find citations for the paper")
+        logger.info(f"The process has finished! It was created {num_citations} citations")
+
+
+@shared_task
 def mark_articles_of_inb_pis(article_ids):
     for article_id in article_ids:
         article_obj = Article.objects.get(id=article_id)
