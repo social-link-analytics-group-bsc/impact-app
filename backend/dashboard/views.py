133318093140b990f4fea5a51f990fe817b7d88b
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-11-06 17:09:47 +0100

    Udpate api calls

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
 from django.conf import settings
+from sci_impact.models import Scientist, Institution
+
+import json
+
+
+def __get_impact_objs():
+    researchers = Scientist.objects.filter(is_pi_inb=True).values('id', 'last_name', 'first_name')
+    impact_objs = []
+    for researcher in researchers:
+        impact_objs.append(
+            {
+                'id': researcher['id'],
+                'name': researcher['first_name'] + ' ' + researcher['last_name'],
+                'type': 'scientist'
+            }
+        )
+    impact_objs.append(
+        {
+            'id': Institution.objects.get(is_inb=True).id,
+            'name': 'INB',
+            'type': 'institution'
+        }
+    )
+    return impact_objs
 
 
 def index(request):
     context = {
-        'server_subfolder': settings.SERVER_SUBFOLDER
+        'server_subfolder': settings.SERVER_SUBFOLDER,
+        'impact_objs': json.dumps(__get_impact_objs()),
+        'institution_name': Institution.objects.get(is_inb=True).name
     }
     return render(request, "main.html", context)
 
 
 def sci_impact(request, **kwargs):
-    impact_ref = kwargs.get('pi')
+    impact_ref = kwargs.get('id')
     context = {
         'impact_obj': impact_ref if impact_ref else '',
-        'show_pis_chart': 1 if not impact_ref else 0,
-        'show_table_papers': 1 if impact_ref else 0,
-        'server_subfolder': settings.SERVER_SUBFOLDER
+        'show_pis_chart': 1 if impact_ref.split('_')[0] == 'institution' else 0,
+        'show_table_papers': 1 if impact_ref.split('_')[0] == 'scientist' else 0,
+        'server_subfolder': settings.SERVER_SUBFOLDER,
+        'impact_objs': json.dumps(__get_impact_objs())
     }
     return render(request, 'sci_impact.html', context)
 
@@ -32,5 +59,5 @@ def social_impact(request):
 
 
 def sci_impact_methodology(request, **kwargs):
-    return render(request, 'sci_impact_methodology.html', {})
+    return render(request, 'sci_impact_methodology.html', {'impact_objs': json.dumps(__get_impact_objs())})
 
