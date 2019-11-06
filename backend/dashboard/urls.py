e0986f018413e47f51ce4c5d814378521914a53c
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-11-06 16:58:28 +0100

    Change suffix of parameters in scientific impact endpoint

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -6,6 +6,6 @@ from dashboard.views import index, sci_impact, sci_impact_methodology
 urlpatterns = [
     path('sci_impact/', sci_impact, name='sci_impact'),  # dashboard scientific impact
     path('sci_impact/methodology/', sci_impact_methodology, name='sci_impact_methodology'),
-    path('sci_impact/<str:pi>/', sci_impact, name='sci_impact'),  # dashboard scientific impact
+    path('sci_impact/<str:id>/', sci_impact, name='sci_impact'),  # dashboard scientific impact
     path('', index, name='dashboard'),  # dashboard
 ]
